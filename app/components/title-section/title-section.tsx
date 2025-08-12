017b9d4fa157c49aca6cb33a708fdbcc436a53a7
WIN-00FG2S0J5MJ\user <filippkotenko@outlook.com>
2025-08-12 17:11:31 +0300

    ww24

--- app/components/title-section/title-section.tsx
+++ app/components/title-section/title-section.tsx
@@ -4,8 +4,16 @@ import { Props } from '@/app/components/title-section/title-section.types';
 export const TitleSection: FC<Props> = ({ children, color }) => {
   return (
     <h2
-      style={{ lineHeight: '0.3', color: color || 'white' }}
-      className="font-blogger text-[clamp(1.0781rem,2.15625vw,1.725rem)] tracking-tight dark:text-white mb-4 leading-tight"
+      style={{ lineHeight: '1.2', color: color || 'white' }}
+      className="
+        font-blogger
+  text-[clamp(1.0781rem,2.15625vw,1.725rem)]
+  tracking-tight
+  dark:text-white
+  mb-4
+  leading-normal
+  sm:leading-tight
+      "
     >
       {children}
     </h2>
